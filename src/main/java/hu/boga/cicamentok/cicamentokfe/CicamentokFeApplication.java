45c13f7d7438e78172d62554c8ca536ba023d776
kunb <balazs.kun@gmail.com>
2022-02-19 07:53:52 +0100

    initial import

---
+++ src/main/java/hu/boga/cicamentok/cicamentokfe/CicamentokFeApplication.java
@@ -0,0 +1,13 @@
+package hu.boga.cicamentok.cicamentokfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicamentokFeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicamentokFeApplication.class, args);
+    }
+
+}
